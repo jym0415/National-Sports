5313e741e6cd3d693c5fc8f31d22e56b44f26873
Limstar12 <phji0326@gmail.com>
2022-05-17 12:32:04 +0900

    전체메뉴노출메뉴삭제

--- theme/basic/head.php
+++ theme/basic/head.php
@@ -35,7 +35,7 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
     <div id="hd_wrapper" class='row justify-content-between align-items-center m-auto py-2 hjinner '>
         <div class="logoBox d-flex align-items-center col-3">                    
             <h1 id="logo" class="col-9">
-                <a href="/joy">
+                <a href="/joy/">
             <?php echo latest('logo', 'logo', 1, 100)?>
                 </a>
             </h1>
@@ -90,42 +90,6 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
                         <li class="gnb_empty">메뉴 준비 중입니다.<?php if ($is_admin) { ?> <a href="<?php echo G5_ADMIN_URL; ?>/menu_list.php">관리자모드 &gt; 환경설정 &gt; 메뉴설정</a>에서 설정하실 수 있습니다.<?php } ?></li>
                     <?php } ?>
                 </ul>
-                <!-- <div id="gnb_all">
-                    <h2>전체메뉴</h2>
-                    <ul class="gnb_al_ul">
-                        <?php
-                        
-                        $i = 0;
-                        foreach( $menu_datas as $row ){
-                        ?>
-                        <li class="gnb_al_li">
-                            <a href="<?php echo $row['me_link']; ?>" target="_<?php echo $row['me_target']; ?>" class="gnb_al_a"><?php echo $row['me_name'] ?></a>
-                            <?php
-                            $k = 0;
-                            foreach( (array) $row['sub'] as $row2 ){
-                                if($k == 0)
-                                    echo '<ul>'.PHP_EOL;
-                            ?>
-                                <li><a href="<?php echo $row2['me_link']; ?>" target="_<?php echo $row2['me_target']; ?>"><?php echo $row2['me_name'] ?></a></li>
-                            <?php
-                            $k++;
-                            }   //end foreach $row2
-
-                            if($k > 0)
-                                echo '</ul>'.PHP_EOL;
-                            ?>
-                        </li>
-                        <?php
-                        $i++;
-                        }   //end foreach $row
-
-                        if ($i == 0) {  ?>
-                            <li class="gnb_empty">메뉴 준비 중입니다.<?php if ($is_admin) { ?> <br><a href="<?php echo G5_ADMIN_URL; ?>/menu_list.php">관리자모드 &gt; 환경설정 &gt; 메뉴설정</a>에서 설정하실 수 있습니다.<?php } ?></li>
-                        <?php } ?>
-                    </ul>
-                    <button type="button" class="gnb_close_btn"><i class="fa fa-times" aria-hidden="true"></i></button>
-                </div>
-                <div id="gnb_all_bg"></div> -->
             </div>
         </nav>       
     </div>
